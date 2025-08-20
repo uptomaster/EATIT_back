6e3473ff8d631566b364fad3c083be2591c76339
gisu-95 <z0170z@naver.com>
2025-08-20 19:02:14 +0900

    250820 19:01 김기수 dto 수정

--- team02-backend/src/main/java/com/bapseguen/app/dto/PromoBoardDTO.java
+++ team02-backend/src/main/java/com/bapseguen/app/dto/PromoBoardDTO.java
@@ -1,5 +1,26 @@
 package com.bapseguen.app.dto;
 
 public class PromoBoardDTO {
-
+	
+	private int postNubmer;
+	private String promoContent;
+	
+	public int getPostNubmer() {
+		return postNubmer;
+	}
+	public void setPostNubmer(int postNubmer) {
+		this.postNubmer = postNubmer;
+	}
+	public String getPromoContent() {
+		return promoContent;
+	}
+	public void setPromoContent(String promoContent) {
+		this.promoContent = promoContent;
+	}
+	@Override
+	public String toString() {
+		return "PromoBoardDTO [postNubmer=" + postNubmer + ", promoContent=" + promoContent + "]";
+	}
+	
+	
 }
