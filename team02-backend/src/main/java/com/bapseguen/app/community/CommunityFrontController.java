fe40b314af03287fddfbe39a17483ad5b28857d0
songeunkyeong99 <ruddlgkxn@gmail.com>
2025-08-22 17:56:42 +0900

    250822 17:56 송은경 communityFrontController 작업중

--- team02-backend/src/main/java/com/bapseguen/app/community/CommunityFrontController.java
+++ team02-backend/src/main/java/com/bapseguen/app/community/CommunityFrontController.java
@@ -44,18 +44,73 @@ public class CommunityFrontController extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 
 		String target = request.getRequestURI().substring(request.getContextPath().length());
-		System.out.println("현재 경로 : " + target);
+		System.out.println("CommunityFrontController 현재 경로 : " + target);
 		Result result = new Result();
 		
 		
+		switch (target) {
+		case "/community/communityMainOk.co":
+			System.out.println("공지사항/이벤트 목록 페이지 처리 요청");
+			result = new CommunityMainOkController().execute(request, response);
+			break;
+		case "/community/viewOwnPostOk.co":
+			System.out.println("공지사항/이벤트 게시글 상세 페이지 처리 요청");
+			result = new ViewOwnPostReadOkController().execute(request, response);
+			break;	
+		case "/community/freeBoardListOk.co":
+			System.out.println("자유게시판 목록 페이지 처리 요청");
+			result = new FreeBoardListOkController().execute(request, response);
+			break;
+		case "/community/writeFreeBoard.co":
+			System.out.println("자유게시판 게시글 작성페이지 이동 요청");
+			result = new WriteFreeBoardController().execute(request, response);
+			break;
+		case "/community/writeFreeBoardOK.co":
+			System.out.println("자유게시판 게시글 작성완료 요청");
+			result = new WriteFreeBoardOKController().execute(request, response);
+			break;
+		case "/community/BoardDeleteOK.co":
+			System.out.println("게시글 삭제 완료 요청");
+			result = new BoardDeleteOKController().execute(request, response);
+			break;
+			
+		case "/community/BoardUpdateOK.co":
+			System.out.println("게시글 수정 페이지 이동 요청");
+			result = new BoardUpdateController().execute(request, response);
+			break;	
+			
+			
+			
+		case "/community/promoBoardListOk.co":
+			System.out.println("홍보게시판 목록 페이지 처리 요청");
+			result = new PromoBoardListOkController().execute(request, response);
+			break;
+		case "/community/writePromoBoard.co":
+			System.out.println("홍보게시판 게시글 작성페이지 이동 요청");
+			result = new WritePromoBoardController().execute(request, response);
+			break;	
+		case "/community/writePromoOKBoard.co":
+			System.out.println("홍보게시판 게시글 작성완료 요청");
+			result = new WritePromoBoardOKController().execute(request, response);
+			break;
+			
+			
+			
+		case "/community/recipeListOk.co":
+			System.out.println("레시피게시판 목록페이지 처리 요청");
+			result = new RecipeListOkController().execute(request, response);
+			break;
+		case "/community/writeRecipeBoard.co":
+			System.out.println("홍보게시판 게시글 작성페이지 이동 요청");
+			result = new WriteRecipeBoardController().execute(request, response);
+			break;
+		case "/community/writeRecipeOKBoard.co":
+			System.out.println("홍보게시판 게시글 작성완료 요청");
+			result = new WriteRecipeBoardOKController().execute(request, response);
+			break;
 		
-		
-		
-		
-		
-		
-		
-		
-	}
+			
+			
+		}
 
 }
