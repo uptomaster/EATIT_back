4ad49f295f3c4687fb2e132dd9de94cafc50b092
uptomaster <lnh929@naver.com>
2025-08-22 09:20:52 +0900

    250822 09:20 이남혁 관리자 로그인 작업중

--- team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginController.java
@@ -1,8 +1,6 @@
 package com.bapseguen.app.admin;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,7 +13,24 @@ public class AdminLoginController implements Execute {
 	public Result execute(HttpServletRequest request, HttpServletResponse response)
     System.out.println("[ADMIN] 관리자 로그인 페이지 요청");
 
-    Result result = new Result();
+	Result result = new Result();
+	Cookie[] cookies = request.getCookies();
+	//사용자가 브라우저에서 서버로 요청을 보낼 때 브라우저는 자신이 가지고 있는 쿠키들을 함께 전송
+	//request.getCookies() 쿠키들을 배열로 꺼내는 메소드
+	
+	if (cookies != null) {
+		//쿠키가 전혀없으면 getCookes()는 null을 반환하기 때문에 null 체크를 먼저 진행함
+		for (Cookie cookie : cookies) {
+			//쿠키 배열이 여러개 있을 수 있으니 for-each문으로 하나씩 꺼낸다
+			if (cookie.getName().equals("memberId")) {
+				//쿠키마다 이름이 존재하는데 memberId라는 이름을 가진 쿠키를 찾는다
+				request.setAttribute("memberId", cookie.getValue());
+				//찾은 memberId 쿠키의 값을 request속성에 넣어준다
+				//${memberId} 형태로 꺼내 쓸수가 있다
+			}
+		}
+	}
+	
     result.setPath("/app/admin/adminLogin.jsp");
     result.setRedirect(false);
     return result;
