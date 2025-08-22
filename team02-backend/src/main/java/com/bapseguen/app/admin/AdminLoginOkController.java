8c72e09cb13659a66a701231391883901206d243
uptomaster <lnh929@naver.com>
2025-08-22 09:11:46 +0900

    250822 0911 이남혁 Admin Mapper, DAO, Controller 작업중

--- team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginOkController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginOkController.java
@@ -8,14 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bapseguen.app.Execute;
 import com.bapseguen.app.Result;
+import com.bapseguen.app.admin.dao.AdminDAO;
 
-public class AdminLoginOkController implements Execute{
+public class AdminLoginOkController implements Execute {
 
 	@Override
 	public Result execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		System.out.println("[ADMIN] 로그인 요청 페이지 시작");
+
+		Result result = new Result();
+		AdminDAO adminDAO = new AdminDAO();
+
 		return null;
 	}
-	
+
 }
