c0e4037a637bcd42917a37cb2a1a2eacc4878ba5
songeunkyeong99 <ruddlgkxn@gmail.com>
2025-08-22 12:03:57 +0900

    250822 12:03 송은경 usermypagefrontcontroller 작업중

--- team02-backend/src/main/java/com/bapseguen/app/userMyPage/UserMyPageFrontController.java
+++ team02-backend/src/main/java/com/bapseguen/app/userMyPage/UserMyPageFrontController.java
@@ -1,11 +1,13 @@
 package com.bapseguen.app.userMyPage;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bapseguen.app.Result;
 
 /**
  * Servlet implementation class userMyPageFrontController
@@ -38,6 +40,19 @@ public class UserMyPageFrontController extends HttpServlet {
 	}
 
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println("현재 경로 : " + target);
+		Result result = new Result();
+		
+		switch (target) {
+		case "/.my":
+			System.out.println("내 정보 수정 페이지 요청");
+			request.getRequestDispatcher("/app/UserMyPage/editUserInfo.jsp").forward(request, response);
+			break;
+		
 		
 	}
 
