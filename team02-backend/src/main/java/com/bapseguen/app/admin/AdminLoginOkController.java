d043772b5926dbe8a9051b22ec858cbbb943c419
uptomaster <lnh929@naver.com>
2025-08-22 09:15:16 +0900

    250822 09:15 이남혁 관리자 로그인 컨트롤러 작업중

--- team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginOkController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/AdminLoginOkController.java
@@ -8,20 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bapseguen.app.Execute;
 import com.bapseguen.app.Result;
-import com.bapseguen.app.admin.dao.AdminDAO;
 
-public class AdminLoginOkController implements Execute {
+public class AdminLoginOkController implements Execute{
 
 	@Override
 	public Result execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		System.out.println("[ADMIN] 로그인 요청 페이지 시작");
-
-		Result result = new Result();
-		AdminDAO adminDAO = new AdminDAO();
-
+		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 }
