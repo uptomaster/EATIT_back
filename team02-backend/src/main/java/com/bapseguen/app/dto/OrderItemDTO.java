716aa2f2700561058647743fd93edf89c5dde0e9
uptomaster <lnh929@naver.com>
2025-08-24 02:57:30 +0900

    250824 02:57 장바구니, 주문 DTO

--- team02-backend/src/main/java/com/bapseguen/app/dto/OrderItemDTO.java
+++ team02-backend/src/main/java/com/bapseguen/app/dto/OrderItemDTO.java
@@ -1,53 +1,36 @@
 package com.bapseguen.app.dto;
 
 public class OrderItemDTO {
+	
+    private int orderItemNumber;        // PK
+    private int ordersNumber;           // FK -> TBL_ORDERS(ORDERS_NUMBER)
+    private Integer itemNumber;         // FK -> TBL_ITEM(ITEM_NUMBER)
+    private int orderItemUnitPrice;     // 단가(주문 시점 가격)
+    private int orderItemQuantity;      // 수량
 
-	private int orderItemNumber;
-	private int ordersNumber;
-	private int itemNumber;
-	private int orderItemUnitPrice;
-	private int orderItemQuantity;
-	
-	public int getOrderItemNumber() {
-		return orderItemNumber;
-	}
-	
-	// getter & setter
-	public void setOrderItemNumber(int orderItemNumber) {
-		this.orderItemNumber = orderItemNumber;
-	}
-	public int getOrderNumber() {
-		return ordersNumber;
-	}
-	public void setOrderNumber(int orderNumber) {
-		this.ordersNumber = orderNumber;
-	}
-	public int getItemNumber() {
-		return itemNumber;
-	}
-	public void setItemNumber(int itemNumber) {
-		this.itemNumber = itemNumber;
-	}
-	public int getOrderItemUnitPrice() {
-		return orderItemUnitPrice;
-	}
-	public void setOrderItemUnitPrice(int orderItemUnitPrice) {
-		this.orderItemUnitPrice = orderItemUnitPrice;
-	}
-	public int getOrderItemQuantity() {
-		return orderItemQuantity;
-	}
-	public void setOrderItemQuantity(int orderItemQuantity) {
-		this.orderItemQuantity = orderItemQuantity;
-	}
-	
-	@Override
-	public String toString() {
-		return "OrderItemDTO [orderItemNumber=" + orderItemNumber + ", ordersNumber=" + ordersNumber + ", itemNumber="
-				+ itemNumber + ", orderItemUnitPrice=" + orderItemUnitPrice + ", orderItemQuantity=" + orderItemQuantity
-				+ "]";
-	}
-	
-	
-	
+    
+    // getter & setter
+    public int getOrderItemNumber() { return orderItemNumber; }
+    public void setOrderItemNumber(int orderItemNumber) { this.orderItemNumber = orderItemNumber; }
+
+    public int getOrdersNumber() { return ordersNumber; }
+    public void setOrdersNumber(int ordersNumber) { this.ordersNumber = ordersNumber; }
+
+    public Integer getItemNumber() { return itemNumber; }
+    public void setItemNumber(Integer itemNumber) { this.itemNumber = itemNumber; }
+
+    public int getOrderItemUnitPrice() { return orderItemUnitPrice; }
+    public void setOrderItemUnitPrice(int orderItemUnitPrice) { this.orderItemUnitPrice = orderItemUnitPrice; }
+
+    public int getOrderItemQuantity() { return orderItemQuantity; }
+    public void setOrderItemQuantity(int orderItemQuantity) { this.orderItemQuantity = orderItemQuantity; }
+
+    @Override
+    public String toString() {
+        return "OrderItemDTO [orderItemNumber=" + orderItemNumber
+                + ", ordersNumber=" + ordersNumber
+                + ", itemNumber=" + itemNumber
+                + ", orderItemUnitPrice=" + orderItemUnitPrice
+                + ", orderItemQuantity=" + orderItemQuantity + "]";
+    }
 }
