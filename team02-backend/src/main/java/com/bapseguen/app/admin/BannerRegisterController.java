15dd3855b7eafe966ddbd2a4724a0d6c8cc3e95a
ericlee1103 <jhoon1103@gmail.com>
2025-08-24 12:49:24 +0900

    Reapply "Merge branch 'main' into ljh"
    This reverts commit ce4db52dc0a17f6933aff4d3a6e7338cbdef3005.

--- team02-backend/src/main/java/com/bapseguen/app/admin/BannerRegisterController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/BannerRegisterController.java
@@ -1,22 +1,28 @@
 package com.bapseguen.app.admin;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.*;
 import com.bapseguen.app.Execute;
 import com.bapseguen.app.Result;
 
-public class BannerRegisterController implements Execute{
+public class BannerRegisterController implements Execute {
+    @Override
+    public Result execute(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	@Override
-	public Result execute(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        System.out.println("[ADMIN] 배너 등록 페이지");
+        Result result = new Result();
+        HttpSession s = request.getSession(false);
+        if (s == null || !"ADMIN".equals(String.valueOf(s.getAttribute("memberType")))) {
 
-	
+            result.setPath(request.getContextPath() + "/admin/login.ad");
+            result.setRedirect(true);
+            return result;
+        }
+
+        result.setPath("/app/admin/banner/bannerRegister.jsp");
+        result.setRedirect(false);
+        return result;
+    }
 }
