ce4db52dc0a17f6933aff4d3a6e7338cbdef3005
ericlee1103 <jhoon1103@gmail.com>
2025-08-24 12:47:12 +0900

    Revert "Merge branch 'main' into ljh"
    This reverts commit 0828a8408672276d410bba3d9df64e433ab5e40b, reversing
    changes made to 27d56384ad8d8576aaf0c380e2c7668cba1eacc2.
    
    250824 12:47 Change commit

--- team02-backend/src/main/java/com/bapseguen/app/admin/BannerUpdateOkController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/BannerUpdateOkController.java
@@ -5,31 +5,19 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.bapseguen.app.Execute;
 import com.bapseguen.app.Result;
-import com.bapseguen.app.dto.BannerDTO;
 
-public class BannerUpdateOkController implements Execute {
-//	아직 미완성
+public class BannerUpdateOkController implements Execute{
+
 	@Override
 	public Result execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		System.out.println("[ADMIN] 배너 수정 처리");
-		Result result = new Result();
-		
-		HttpSession s = request.getSession(false);
-		if (s == null || !"ADMIN".equals(String.valueOf(s.getAttribute("memberType")))) {
-			Result r = new Result();
-			r.setPath(request.getContextPath() + "/admin/login.ad");
-			r.setRedirect(true);
-			return r;
-		}
-
-		BannerDTO dto = new BannerDTO();
-		return result;
-
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+	
+	
 }
