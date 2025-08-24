8afe111403bf2597d9f73a83189e28702d707b7e
ericlee1103 <jhoon1103@gmail.com>
2025-08-24 12:58:42 +0900

    Revert "Merge pull request #38 from uptomaster/lnh"
    This reverts commit c0bccb78be4280f12950bbb8f63b16c43adb4b71, reversing
    changes made to a4db611e2824294aea065b6ba55f863f48ea2fb6.
    
    250824 12:58 되돌리기

--- team02-backend/src/main/java/com/bapseguen/app/admin/BannerRegisterOkController.java
+++ team02-backend/src/main/java/com/bapseguen/app/admin/BannerRegisterOkController.java
@@ -1,31 +1,23 @@
 package com.bapseguen.app.admin;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.bapseguen.app.Execute;
 import com.bapseguen.app.Result;
-import com.bapseguen.app.admin.dao.AdminDAO;
-import com.bapseguen.app.dto.BannerDTO;
 
-public class BannerRegisterOkController implements Execute {
+public class BannerRegisterOkController implements Execute{
 
-//	아직 미완성
-    @Override
-    public Result execute(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-        System.out.println("[ADMIN] 배너 등록 처리");
-        Result result = new Result();
-        
-        HttpSession s = request.getSession(false);
-        if (s == null || !"ADMIN".equals(String.valueOf(s.getAttribute("memberType")))) {
-
-        	result.setPath(request.getContextPath() + "/admin/login.ad");
-        	result.setRedirect(true);
-            return result;
-        }
-
-        return result;
-    }
+	
+	
 }
