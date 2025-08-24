1642ba53248fb44f5d31b8818a4117f77a910d22
gisu-95 <z0170z@naver.com>
2025-08-24 13:48:59 +0900

    250824 13:25 김기수 아찾,비찾 sms인증 제외 안료

---
+++ team02-backend/src/main/java/com/bapseguen/app/findId/FindIdOkController.java
@@ -0,0 +1,35 @@
+package com.bapseguen.app.findId;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bapseguen.app.Execute;
+import com.bapseguen.app.Result;
+import com.bapseguen.app.findId.dao.FindIdDAO;
+
+
+public class FindIdOkController implements Execute{
+
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Result result = new Result();
+		FindIdDAO findIdDAO = new FindIdDAO();
+		
+		String name = request.getParameter("findIdAuth_input_name");
+		String phone = request.getParameter("findIdAuth_input_phone");
+		String id = findIdDAO.findId(name, phone);
+		result.setRedirect(true);
+		if(id == null) {
+			result.setPath(request.getContextPath()+ "/app/findId/findUserIdFail.jsp");
+		}else{
+			result.setPath(request.getContextPath() + "/app/login/login.jsp");
+		}
+		return result;
+	}
+
+}
